c1aa7a383b7f40578f9693bdc1192ec273c9008f
David Do <ewisozk4@asu.edu>
2026-09-13 17:26:57 +0700

    test(authoring): lock portable artifact digest

--- test/e2e/authoring-determinism.test.ts
+++ test/e2e/authoring-determinism.test.ts
@@ -5,7 +5,7 @@ import { tmpdir } from 'node:os';
 import { join } from 'node:path';
 import { packSkill } from '../../packages/authoring/src/index.ts';
 
-const EXPECTED_ARTIFACT_SHA256 = '0000000000000000000000000000000000000000000000000000000000000000';
+const EXPECTED_ARTIFACT_SHA256 = '47875f44bb64387c6e43525ec1e3fad8c175aff17dfe88060dfeb3a7f516b23e';
 
 test('normalized authoring fixture has one portable artifact digest', async () => {
   const root = await mkdtemp(join(tmpdir(), 'auno-determinism-source-'));
