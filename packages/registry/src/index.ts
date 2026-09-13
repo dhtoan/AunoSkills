75c2b937ceedad501c8a5e5fbd61a361708a24d0
David Do <ewisozk4@asu.edu>
2026-09-13 12:46:35 +0700

    feat(registry): export official registry bootstrap

--- packages/registry/src/index.ts
+++ packages/registry/src/index.ts
@@ -6,5 +6,6 @@ export * from './validate.ts';
 export * from './build.ts';
 export * from './unsigned-build.ts';
 export * from './delegated-build.ts';
+export * from './official.ts';
 export * from './trust.ts';
 export * from './auth.ts';
