421b3d072ceddc44faaaaa037a7d32eb8d43d638
David Do <ewisozk4@asu.edu>
2026-09-13 15:38:37 +0700

    feat(authoring): export inspection and validation helpers

--- packages/authoring/src/index.ts
+++ packages/authoring/src/index.ts
@@ -2,4 +2,7 @@ export * from './types.ts';
 export * from './identity.ts';
 export * from './init.ts';
 export * from './inventory.ts';
+export * from './capabilities.ts';
+export * from './dependencies.ts';
 export * from './validate.ts';
+export * from './inspect.ts';
